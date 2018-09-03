4950b9af4948e681fe9282dc4cbce1357dac3b13
Drew Keller <drew@wimpyprogrammer.com>
2018-09-02 21:17:20 -0500

    Add word list parser
    Add parseString() to convert a user's raw input into an array of words
    to process.

---
+++ src/utils/wordList.ts
@@ -0,0 +1,26 @@
+export enum WhitespaceHandling {
+	Preserve,
+	TrimLeadingAndTrailing,
+}
+
+export function parseString(
+	raw: string,
+	delimiter: string,
+	whitespaceHandling: WhitespaceHandling
+): string[] {
+	if (!raw) {
+		return [];
+	}
+
+	if (!delimiter) {
+		return [raw];
+	}
+
+	let words = raw.split(delimiter);
+
+	if (whitespaceHandling === WhitespaceHandling.TrimLeadingAndTrailing) {
+		words = words.map(word => word.trim());
+	}
+
+	return words;
+}
