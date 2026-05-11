366175d78b026dc66dd49853e28dad667479c7d9
Drew Scheller <drew@wimpyprogrammer.com>
2026-05-11 02:04:31 -0500

    Suppress ESLint warning "Forbidden non-null assertion"

--- packages/strings-to-regex/src/utils/trie.ts
+++ packages/strings-to-regex/src/utils/trie.ts
@@ -35,6 +35,7 @@ function mergeGroups(headChar: Char, tailGroup: CharTrie): CharTrie {
 		return new Map([[headChar, tailGroup]]) as CharTrie;
 	}
 
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 	const [onlyTail, onBranch] = tailGroup.entries().next().value!;
 	return new Map([[headChar + onlyTail, onBranch]]) as CharTrie;
 }
